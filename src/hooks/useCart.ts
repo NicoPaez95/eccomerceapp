b38cfe1b779a4bfd26c58247d5df999322b0a81b
NicoPaez95 <nicopaezcba4@gmail.com>
2025-06-26 15:48:42 -0300

    hooks

---
+++ src/hooks/useCart.ts
@@ -0,0 +1,13 @@
+// src/hooks/useCart.ts
+/*import { useContext } from "react";
+import { CarContext } from "../context/CarContext";
+
+export const useCart = () => {
+  const context = useContext(CarContext);
+
+  if (!context) {
+    throw new Error("useCart debe usarse dentro de un CarProvider");
+  }
+
+  return context;
+};*/
