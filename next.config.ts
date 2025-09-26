9a619b269644a7a2dfcb2848c1a07a86eee8d7b5
NicoPaez95 <nicopaezcba4@gmail.com>
2025-09-26 07:42:53 -0300

    update-bugs-config

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,9 @@
 const nextConfig = {
   output: 'standalone',
   basePath: '/shop',
-  // Elimina esta línea, el PORT se define en el script start
-  // env: {
-  //   PORT: '4000'
-  // },
-  // Agrega esta configuración importante
-  trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/shop/:path*',
-        destination: '/:path*',
-      },
-    ];
+  serverRuntimeConfig: {
+    port: parseInt(process.env.PORT ?? '4000', 10),
+    hostname: '0.0.0.0'
   }
 }
 
