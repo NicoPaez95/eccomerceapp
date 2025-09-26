0ddae52c9ac0dbe3e6333fbb16fe05c58d42854d
NicoPaez95 <nicopaezcba4@gmail.com>
2025-09-26 08:00:51 -0300

    fixing-bugs-package-json

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  basePath: '/shop',
-  serverRuntimeConfig: {
-    port: parseInt(process.env.PORT ?? '4000', 10),
-    hostname: '0.0.0.0'
-  }
+  // output: 'standalone', // ← ELIMINAR esta línea
+  basePath: '/shop'
 }
 
 module.exports = nextConfig
